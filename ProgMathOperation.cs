919243d86f651ebe925f2dcfdb632f7db741fbe1
thanhan7501 <anntgch190372@fpt.edu.vn>
2020-09-24 17:30:20 +0700

    [MathOperation] add new

---
+++ ProgMathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperation
+{
+    public class ProgMathOperation
+    {
+        public static void Run()
+        {
+            MathOperation mo = new MathOperation();
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+            Console.ReadKey();
+        }
+    }
+}
